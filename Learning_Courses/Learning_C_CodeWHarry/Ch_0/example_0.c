75f55b05998409d6f7865f94a8267b139c533e6c
Avi <avi20060723@gmail.com>
2025-08-05 22:53:34 +0530

    Add files via upload

---
+++ Learning_Courses/Learning_C_CodeWHarry/Ch_0/example_0.c
@@ -0,0 +1,27 @@
+// Chapter 0
+
+// C Programming Language - First Program
+
+#include<stdio.h> // Header file → Preprocessor directive : includes the funtions required by the compiler to comper the code.
+
+int main(){ 
+
+    // int , tells us that the return value of a function is an integer 
+    // main() is a funtion
+    // {...} represent the start and end of the function
+
+    printf("Hello World"); // printf = print the thing
+
+    // anything inside "..." is a string
+
+    return 0; // return = the end of function and return the value 0
+}
+
+// Boilerplate code for C programs:
+/*
+#include <DIRECTIVE>
+int main(){
+  [body of the code];
+  return 0;
+}
+*/
