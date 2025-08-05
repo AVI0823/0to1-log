ddcb0cdfceefff77d453ead8d4120795506dd354
Avi <avi20060723@gmail.com>
2025-08-05 23:03:14 +0530

    Delete Learning_Courses/Learning_C_CodeWHarry/CH_5 directory

--- Learning_Courses/Learning_C_CodeWHarry/CH_5/math_library.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main(){
-    int a = 5;
-    printf("The area of the squar with side %d is : %.1f \n",a,pow(a,2));
-    return 0;
-}
--- Learning_Courses/Learning_C_CodeWHarry/CH_5/tempCodeRunnerFile.c
+++
@@ -1,4 +0,0 @@
-int n;
-    // printf("Enter the number : ");
-    // scanf("%d",&n);
-    // printf("The %dth term in fibonachi is : %d",n,fibonacci(n));
